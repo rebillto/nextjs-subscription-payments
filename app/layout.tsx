cfe0b0d1e8b576714e52aa3a3f8449f66ad50d14
German <germangguerci@gmail.com>
2023-08-18 12:51:56 -0600

    chore(init): set up project structure and dependencies

---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Footer from '@/components/ui/Footer';
+import Navbar from '@/components/ui/Navbar';
+import { PropsWithChildren } from 'react';
+import 'styles/main.css';
+
+const meta = {
+  title: 'Next.js Rebill Subscription Starter',
+  description: 'Brought to you by Vercel and Rebill.',
+  cardImage: '/og.png',
+  robots: 'follow, index',
+  favicon: '/favicon.ico',
+  //todo update url. 
+  url: 'https://rebill-starter.vercel.app',
+  type: 'website'
+};
+
+export const metadata = {
+  title: meta.title,
+  description: meta.description,
+  cardImage: meta.cardImage,
+  robots: meta.robots,
+  favicon: meta.favicon,
+  url: meta.url,
+  type: meta.type,
+  openGraph: {
+    url: meta.url,
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage,
+    type: meta.type,
+    site_name: meta.title
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@vercel',
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage
+  }
+};
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children
+}: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body className="bg-black loading">
+          <UserProvider>
+            <Navbar />
+            <main
+              id="skip"
+              className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+            >
+              {children}
+            </main>
+            <Footer />
+          </UserProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/ui/Navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+import Logo from '@/components/icons/Logo';
+
+import s from './Navbar.module.css';
+
+export default function Navbar() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+
+  return (
+    <nav className={s.root}>
+      <a href="#skip" className="sr-only focus:not-sr-only">
+        Skip to content
+      </a>
+      <div className="max-w-6xl px-6 mx-auto">
+        <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
+          <div className="flex items-center flex-1">
+            <Link href="/" className={s.logo} aria-label="Logo">
+              <Logo />
+            </Link>
+            <nav className="hidden ml-6 space-x-2 lg:block">
+              <Link href="/" className={s.link}>
+                Pricing
+              </Link>
+              {user && (
+                <Link href="/account" className={s.link}>
+                  Account
+                </Link>
+              )}
+            </nav>
+          </div>
+          <div className="flex justify-end flex-1 space-x-8">
+            {user ? (
+            <a href="/api/auth/logout" className={s.link}>
+              Sign Out
+            </a>
+            ) : (
+              <a href="/api/auth/login" className={s.link}>
+                Sign in
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
