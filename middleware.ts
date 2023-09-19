3fbf09f7490c53bf77e90ae83e6c36a3ed1c9479
German <germangguerci@gmail.com>
2023-09-18 19:35:32 -0600

    feature: middleware api validation

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
  
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'es', 'pt'],
  
@@ -8,8 +9,21 @@ export default createMiddleware({
   defaultLocale: 'en'
 });
  
+export default async function middleware(req: NextRequest) {
+  const url = req.nextUrl
+  const { pathname } = url
+  if (pathname.startsWith(`/api/`) && !pathname.includes("webhooks")) {
+    const referer = req.headers.get("referer");
+    if ((!referer?.includes(process.env.NEXT_PUBLIC_SITE_URL as string) && !pathname.includes(`/auth`) ) || (!referer && !pathname.includes(`/auth`))) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+      }
+    return NextResponse.next()
+  }else{
+    return intlMiddleware(req)
+
+  }
+}
+
 export const config = {
-  // Skip all paths that should not be internationalized. This example skips the
-  // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ['/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)'],
 };
